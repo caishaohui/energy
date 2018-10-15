1dc8ee68fa294cb8fcafd89676230575592a4c77
huangdongwen <604084670@qq.com>
2018-10-15 19:50:59 +0800

    任务动态模块布局

---
+++ src/pages/index/views/monitoring-center/compoents/task-module.vue
@@ -0,0 +1,83 @@
+<template>
+    <div id="task_module">
+        <div class="box_task">
+            <div class="box_task_title">
+                <span class="box_task_title_sign"></span>
+                <p>任务动态</p>
+                <img :src="rightArrowsIcon" alt="">
+            </div>
+            <div class="box_task_content">
+               <ul>
+                  <li>
+                      <img :src="taskCustomerIcon" alt="">
+                      <div class="box_task_content_site">
+                          <div >巡检</div>
+                          <div>福田</div>
+                      </div>
+                      <div class="box_task_content_achieve">待审批</div>
+                      <div class="clear"></div>
+                  </li>
+                   <li>
+                       <img :src="taskCustomerIcon" alt="">
+                       <div class="box_task_content_site">
+                           <div >巡检</div>
+                           <div>福田</div>
+                       </div>
+                       <div class="box_task_content_achieve">待审批</div>
+                       <div class="clear"></div>
+                   </li>
+                   <li>
+                       <img :src="taskCustomerIcon" alt="">
+                       <div class="box_task_content_site">
+                           <div >巡检</div>
+                           <div>福田</div>
+                       </div>
+                       <div class="box_task_content_achieve">待审批</div>
+                       <div class="clear"></div>
+                   </li>
+                   <li>
+                       <img :src="taskCustomerIcon" alt="">
+                       <div class="box_task_content_site">
+                           <div >巡检</div>
+                           <div>福田</div>
+                       </div>
+                       <div class="box_task_content_achieve">待审批</div>
+                       <div class="clear"></div>
+                   </li>
+                   <li>
+                       <img :src="taskCustomerIcon" alt="">
+                       <div class="box_task_content_site">
+                           <div >巡检</div>
+                           <div>福田</div>
+                       </div>
+                       <div class="box_task_content_achieve">待审批</div>
+                       <div class="clear"></div>
+                   </li>
+               </ul>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    import rightArrowsIcon from  '@/assets/icon/rightArrows.png'
+    import taskCustomerIcon from  '@/assets/icon/taskCustomer.png'
+    export default {
+        name: "task-module",
+        data(){
+            return{
+                rightArrowsIcon,
+                taskCustomerIcon
+            }
+        }
+    }
+</script>
+
+<style rel="stylesheet/scss" lang="scss"  scoped>
+    @import "../../../styles/monitoring.scss";
+    html{
+        font-size: 100px;
+    }
+
+</style>
